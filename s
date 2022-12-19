cbfdfa99e78afc989d53a5fae571f2dcc9bf99dd
seivadarve <flowreshe.seiva.d.arve@gmail.com>
2022-12-19 02:51:25 +0000

    Update ss.sh + Add: Shiva-Sutras PDF

---
+++ s
@@ -0,0 +1,16 @@
+tput setaf 4
+echo "
+Devi Asks:
+"
+
+tput sgr0
+echo "O Shiva, what is your reality?
+What is this wonder-filled universe?
+What constitutes seed?
+Who centers the universal wheel?
+What is this life beyond form pervading forms?
+How may we enter it fully,
+above space and time,
+names and descriptions?
+Let my doubts be cleared! 
+"
--- ss.sh
+++ ss.sh
@@ -9,6 +9,362 @@
 
 # Function to list all the Shiva Sutras by number
 
+function f_display_all {
+echo "
+Devi Asks:
+O Shiva, what is your reality?
+What is this wonder-filled universe?
+What constitutes seed?
+Who centers the universal wheel?
+What is this life beyond form pervading forms?
+How may we enter it fully,
+above space and time,
+names and descriptions?
+Let my doubts be cleared! 
+"
+}
+":
+Shiva Sutra #1: 
+ > Radiant One, this experience may dawn 
+between two breaths. After breath comes in (down) and just before turning up (out) – the beneficence.54 
+
+Shiva Sutra #2: 
+ > As breath turns from down to up, and again as breath curves up to down – through both these turns, realize. 
+
+Shiva Sutra #3: 
+ > Or, whenever in-breath and out-breath fuse, At this instant touch the energy-less, energy-filled center. 
+
+Shiva Sutra #4: 
+ > Or, when breath is all out (up) and stopped of itself, or all in (down) and stopped – in such universal pause, one’s small self vanishes. This is difficult only for 
+the impure. 
+
+Shiva Sutra #5: 
+ > Attention between eyebrowki?s, let mind be before thought. Let form fill with breath essence to the top of the head and there shower as light. 
+
+Shiva Sutra #6: 
+ > When in worldly activities, keep attention between two breaths, and so practicing, in a few days be born anew. 
+
+Shiva Sutra #7: 
+ > With intangible breath in center of forehead, as this reaches the heart at the moment of sleep, have direction over dreams and over death itself. 
+
+Shiva Sutra #8: 
+ > With utmost devotion, center on the two junctions of breath and know the knower. 
+
+Shiva Sutra #9: 
+ > Lie down as dead. Enraged in wrath, stay so. Or stare without moving an eyelash. Or suck something and become the sucking. 
+
+Shiva Sutra #10:
+ > While being caressed, Sweet Princess, enter the caress as everlasting life. 
+
+Shiva Sutra #11:
+ > Stop the doors of the senses when feeling the creeping of an ant. Then. 
+
+Shiva Sutra #12:
+ > When on a bed or a seat, let yourself become weightless, beyond mind. 
+
+Shiva Sutra #13:
+ > Or, imagine the five coloured circles of the peacock tail to be your five senses in illimitable space. Now let their beauty melt within. Similarly, at any point in space or on the wall – untill the point dissolves. Then your wish for another comes true. 
+
+Shiva Sutra #14:
+ > Place your whole attention in the nerve, delicate as the lotus thread, in the center of your spinal column. In such be transformed. 
+
+Shiva Sutra #15:
+ > Closing the seven openings of the head with your hands, a space between your eyes becomes all-inclusive. 
+
+Shiva Sutra #16:
+ > Blessed One, as senses are absorbed in the Heart, reach the center of the lotus. 
+
+Shiva Sutra #17:
+ > Unminding mind, keep in the middle – until. 
+
+Shiva Sutra #18:
+ > Look lovingly at some object. Do not go to another object. Here in the middle of the object – the blessing (let the blessing descend on you - Nithyananda). 
+
+Shiva Sutra #19:
+ > Without support for feet or hands, sit only on the buttocks. Suddenly the centering. 
+
+Shiva Sutra #20:
+ > In a moving vehicle, by rhythmically swaying, experience. Or in a still vehicle, by letting yourself swing in slowing invisible circles. 
+
+Shiva Sutra #21:
+ > Pierce some part of you nectar filled form with a pin, and gently enter the piercing and attain to the inner purity. 
+
+Shiva Sutra #22:
+ > Let attention be at a place where you are seeing some past happening, and even your form, having lost its present characteristics, is transformed. 
+
+Shiva Sutra #23:
+ > Feel an object before you. Feel the absence of all other objects but this one. Then leaving aside the object-feeling And the absence-feeling, Realize. 
+
+Shiva Sutra #24:
+ > When a mood against someone or for someone arises, Do not place it on the person in question, But remain centered. 
+
+Shiva Sutra #25:
+ > Just as you have the impulse To do something, Stop. 
+
+Shiva Sutra #26:
+ > When some desire comes, consider it. Then, suddenly, quit it. 
+
+Shiva Sutra #27:
+ > Roam about until exhausted and then, dropping to the ground, in this dropping be whole. 
+
+Shiva Sutra #28:
+ > Suppose you are gradually being deprived of strength or of knowledge. At the instant of deprivation, transcend. 
+
+Shiva Sutra #29:
+ > Devotion frees. 
+
+Shiva Sutra #30:
+ > Eyes closed, See your inner being in detail. Thus see your true nature. 
+
+Shiva Sutra #31:
+ > Look upon a bowl without seeing the sides or the material. In a few moments become aware. 
+
+Shiva Sutra #32:
+ > See as if for the first time A beauteous person Or an ordinary object. 
+
+Shiva Sutra #33:
+ > Simply by looking into the blue sky beyond the clouds, the serenity. 
+
+Shiva Sutra #34:
+ > Listen while the ultimate mystical teaching is imparted. Eyes still, without blinking, at once, become absolutely free. 
+
+Shiva Sutra #35:
+ > At the edge of a deep well look steadily into its depths until – the wondrousness. (... until the wonderessness happens. by nithyananda) 
+
+Shiva Sutra #36:
+ > Look upon some object, Then slowly withdraw your sight from it, then slowly withdraw your thought from it. Then. 
+
+Shiva Sutra #37:
+ > Devi, Imagine Sanskrit letters in these Honey filled foci of awareness, First as letters, The more subtly as sounds, Then as most subtle feeling. Then, leaving them aside, be free. 
+
+Shiva Sutra #38:
+ > Bathe in the center of sound, As in the continuous sound of a waterfall. Or, by putting the fingers in the ears, Hear the sound of sounds. 
+
+Shiva Sutra #39:
+ > Intone a sound, As AUM… Slowly, As sound enters soundfulness, So do you. 
+
+Shiva Sutra #40:
+ > In the beginning and gradual refinement of the sound of any letter, Awake. 
+
+Shiva Sutra #41:
+ > While listening to stringed instruments, Hear their composite central sound; Thus omnipresence. 
+
+Shiva Sutra #42:
+ > Intone a sound audibly, Then less and less audibly as feeling deepens into this silent harmony. 
+
+Shiva Sutra #43:
+ > With mouth slightly open, Keep mind in the middle of the tongue. Or, as breath comes silently in, Feel the sound ‘HH’. 
+
+Shiva Sutra #44:
+ > Center on the sound ‘AUM’ Without any ‘A’ or ‘M’. 
+
+Shiva Sutra #45:
+ > Silently intone a word ending in ‘AH’. Then in the ‘HH’, Effortlessly, the spontaneity. 
+
+Shiva Sutra #46:
+ > Stopping ears by pressing And the rectum by contracting, Enter the sound. 
+
+Shiva Sutra #47:
+ > Enter the sound of your name And, through this sound, All sounds. 
+
+Shiva Sutra #48:
+ > At the start of sexual union Keep attentive on the fire in the beginning, And so continuing, Avoid the embers in the end. 
+
+Shiva Sutra #49:
+ > When in such embrace your senses are shaken as leaves, Enter this shaking. 
+
+Shiva Sutra #50:
+ > Even remembering union, Without the embrace, Transformation. 
+
+Shiva Sutra #51:
+ > On joyously seeing a long-absent friend, Permeate this joy. 
+
+Shiva Sutra #52:
+ > When eating or drinking, Become the taste of food or drink, And be filled. 
+
+Shiva Sutra #53:
+ > O lotus eyed one, Sweet of touch, When singing, seeing, tasting, Be aware you are and discover the ever living. 
+
+Shiva Sutra #54:
+ > Wherever satisfaction is found, In whatever act, Actualize this. 
+
+Shiva Sutra #55:
+ > At the point of sleep, When the sleep has not yet come and the external wakefulness vanishes, At this point Being is revealed. 
+
+Shiva Sutra #56:
+ > Illusions deceive, Colors circumscribe, Even divisibles are indivisible (- Look and become s
+Shiva - nithyananda's translation) 
+
+Shiva Sutra #57:
+ > In moods of extreme desire Be undisturbed. 
+
+Shiva Sutra #58:
+ > This so-called universe Appears as a juggling, A picture show. To be happy, look upon it so. 
+
+Shiva Sutra #59:
+ > O Beloved, Put attention neither on pleasure nor on pain, But between these. 
+
+Shiva Sutra #60:
+ > Objects and desires Exist in me as in others. So accepting, Let them be transformed. 
+
+Shiva Sutra #61:
+ > As waves come with water And flames with fire, So the Universal waves with us. 
+
+Shiva Sutra #62:
+ > Wherever your mind is wandering, Internally or externally, At this very place, this. 
+
+Shiva Sutra #63:
+ > When vividly aware Through some particular sense, Keep in the awareness. 
+
+Shiva Sutra #64:
+ > At the start of sneezing, during fright, In anxiety, above a chasm, flying in battle, In extreme curiosity, at the beginning of hunger, At the end of hunger, Be uninterruptedly aware. 
+
+Shiva Sutra #65:
+ > The purity of other teachings Is an impurity to us. In reality, Know nothing as pure or impure. 
+
+Shiva Sutra #66:
+ > Be the unsame same to friend as to stranger, in honor and dishonor. 
+
+Shiva Sutra #67:
+ > Here is the sphere of change, change, change. Through change consume change. 
+
+Shiva Sutra #68:
+ > As a hen mothers her chicks, mother particular knowings, particular doings, in reality. 
+
+Shiva Sutra #69:
+ > Since, in truth, Bondage and freedom are relative, These words are only for those Terrified with the universe. This universe is a reflection of minds. As you see many suns in water from one sun, So see bondage and liberation. 
+
+Shiva Sutra #70:
+ > Consider your essence as light rays From center to center up the vertebrae, And so rises “livingness” in you. 
+
+Shiva Sutra #71:
+ > Or in the spaces between, Feel this as lightning. 
+
+Shiva Sutra #72:
+ > Feel the cosmos as a translucent ever-living presence. 
+
+Shiva Sutra #73:
+ > In summer when you see the entire sky endlessly clear, Enter such clarity. 
+
+Shiva Sutra #74:
+ > Shakti, See all space as if already Absorbed in your own head In the brilliance. 
+
+Shiva Sutra #75:
+ > Waking, sleeping, dreaming, Know you as light. 
+
+Shiva Sutra #76:
+ > In rain during a black night, Enter that blackness as the form of forms. 
+
+Shiva Sutra #77:
+ > When a moonless rainy night is not present, close your eyes, see blackness. So, faults disappear forever. 
+
+Shiva Sutra #78:
+ > Whenever your attention alights, At this very point, Experience. 
+
+Shiva Sutra #79:
+ > Focus on fire rising through your form From the toes up Until the body burns to ashes, But not you. 
+
+Shiva Sutra #80:
+ > Meditate On the make believe world as burning to ashes, And become being above human. 
+
+Shiva Sutra #81:
+ > As, subjectively, letters flow into words and words into sentences, and as, objectively, circles flow into worlds and worlds into principles, find at last these converging in our being. 
+
+Shiva Sutra #82:
+ > Feel: my thought, I-ness, internal organs – me. 
+
+Shiva Sutra #83:
+ > Before desire And before knowing, How can I say I am? Consider. Dissolve in the beauty. 
+
+Shiva Sutra #84:
+ > Toss attachment for body aside, Realizing I am everywhere. One who is everywhere is joyous. 
+
+Shiva Sutra #85:
+ > Thinking no thing will limited-self unlimit. 
+
+Shiva Sutra #86:
+ > Suppose you contemplate something beyond perception, Beyond grasping, Beyond not being – you. 
+
+Shiva Sutra #87:
+ > I am existing. This is mine. This is this. O, beloved, even in such know illimitably. 
+
+Shiva Sutra #88:
+ > Each thing is perceived through knowing. The self shines in space through knowing. Perceive one being as knower and known. 
+
+Shiva Sutra #89:
+ > Beloved, At this moment, Let mind, knowing, breath, form, Be included. 
+
+Shiva Sutra #90:
+ > Touching eyeballs as a feather, Lightness between them opens into the heart And there permeates the cosmos. 
+
+Shiva Sutra #91:
+ > Kind Devi, Enter etheric presence Pervading far above and below your form. 
+
+Shiva Sutra #92:
+ > Put mindstuff in such inexpressible fineness Above, Below and in your heart. 
+
+Shiva Sutra #93:
+ > Consider any area of your present form as limitlessly spacious. 
+
+Shiva Sutra #94:
+ > Feel your substance, Bones, flesh, blood, Saturated with cosmic essence. 
+
+Shiva Sutra #95:
+ > Feel the fine qualities of creativity Permeating your breasts And assuming delicate configurations. 
+
+Shiva Sutra #96:
+ > Abide in some place endlessly spacious, clear of trees, hills, habitations. Thence comes the end of mind pressures. 
+
+Shiva Sutra #97:
+ > Consider the plenum to be your own body of bliss. 
+
+Shiva Sutra #98:
+ > In any easy position gradually pervade an area between the armpits into great peace. 
+
+Shiva Sutra #99:
+ > Feel yourself as pervading all directions, Far, near. 
+
+Shiva Sutra #100
+ > : The appreciation of objects and subjects is the same for an enlightened as for an unenlightened person. The former has one greatness: He remains in the subjective mood, Not lost in things. 
+
+Shiva Sutra #101
+ > : Believe omniscient, omnipotent, pervading. 
+
+Shiva Sutra #102
+ > : Imagine spirit simultaneously within and around you until the entire universe spiritualizes. 
+
+Shiva Sutra #103
+ > : With your entire consciousness In the very start of desire, of knowing, know. 
+
+Shiva Sutra #104
+ > : O Shakti, Each particular perception is limited, Disappearing in omnipotence. 
+
+Shiva Sutra #105
+ > : In truth forms are inseparate. Inseparate are omnipresent being and your own form. Realize each as made of this consciousness. 
+
+Shiva Sutra #106
+ > : Feel the consciousness of each person as your own consciousness. So, leaving aside concern for your self, Become each being. 
+
+Shiva Sutra #107
+ > : This consciousness exists as each being, And nothing else exists. 
+
+Shiva Sutra #108
+ > : This consciousness is the spirit of guidance of each one. Be this one. 
+
+Shiva Sutra #109
+ > : Suppose your passive form to be an empty room with walls of skin – empty. 
+
+Shiva Sutra #110
+ > : Gracious One, play. The universe is an empty shell Wherein your mind frolics infinitely. 
+
+Shiva Sutra #111
+ > : Sweet heartened One, Meditate on knowing and not-knowing, Existing and non-existing. Then leave both aside that you may be. 
+
+Shiva Sutra #112
+ > : Enter space, support less, eternal, still.
+heredoc
+}
+'
 case $1 in
    -g | --grep)
       # Searches for text inside the 'vigyan bhairav tantra' document
@@ -34,5 +390,8 @@ case $1 in
       ;;
       1) echo "Shiva Sutra #1:"
          echo "   > Sutra..."
-   ;;
+      ;;
+      *)
+        f_display_all
+      ;;
 esac
